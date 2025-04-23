4435b53bb4481103caad09b23dec64b981541cc8
AlimRM2022 <116039456+AlimRM2022@users.noreply.github.com>
2025-04-23 18:29:41 -0400

    Initial Commit

---
+++ Freestyle/FreestyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreestyleApp.swift
+//  Freestyle
+//
+//  Created by Stalim Rivero on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FreestyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
